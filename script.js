fc381ea0dace86477dd42744719260076cdb1447
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2018-12-12 19:36:46 -0500

    Updated autocomplete, initMap, and eventListener

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ var infowindow;
 
 function loadPage() {
   eventListner();
+  initAutocomplete();
   initMap();
+}
+
+function initAutocomplete () {
+  var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(40.712775,-74.005973),
+    new google.maps.LatLng(41, 75));
+
+  var input = document.getElementById('searchTextField');
+  var options = {
+    bounds: defaultBounds,
+    types: ['establishment']
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+
+}
+
+function initMap(searchTerm) {
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.712, lng: -74.005},
+    zoom: 8
+    });
 }
 
 function eventListner() {
@@ -14,18 +38,24 @@ function eventListner() {
   console.log('Clicked!');
   event.preventDefault();
   console.log('Event prevented!');
-  let userSearchTerm = $('#js-search-input').val();
-  initMap(userSearchTerm);
-  // getUserInput(userSearchTerm);
+  let userSearchTerm = $('#searchTextField').val();
+  updateMap(userSearchTerm);
   });
 }
 
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-      });
+function updateMap(searchTerm) {
+  console.log(`I passed(${searchTerm}) to updateMap`);
+  console.log(searchTerm);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.712, lng: -74.005},
+    zoom: 8
+    });
 
+  var marker = new google.maps.Marker({
+   position: searchTerm,
+   map: map,
+   title: 'Hello World!'
+});
 }
 
 $(loadPage);
