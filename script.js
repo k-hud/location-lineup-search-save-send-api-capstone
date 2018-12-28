32d1bd4161049c915bdbef1f26c9b6a745fce3ad
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2018-12-28 14:12:13 -0500

    Change to Maps API from places
    This will change from Places to Maps API. But dont want to do this. Will revert back to previous commit.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 var map;
 var service;
 var infowindow;
-var marker;
+var markers = [];
 var place;
 
 const savedPlaces = [];
@@ -10,75 +9,92 @@ const savedPlaces = [];
 function loadPage() {
   loadList();
   initMap();
+  console.log(`Function loadPage() was loaded.`);
 }
 
 function loadList() {
-  console.log("I was loaded.");
-  $('#js-clear-list').on('click', event => {
-    
-    $('.js-saved-list').empty();
+  console.log(`Function loadList() was loaded.`);
+  $("#js-clear-list").on("click", event => {
+    $(".js-saved-list").empty();
+    console.log(`Clear list ran.`);
   });
 }
 
 function initMap() {
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.712, lng: -74.005},
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.712, lng: -74.005 },
     zoom: 8
-    });
+  });
 
-    var input = document.getElementById('searchTextField');
-    var options = {
-      // bounds: defaultBounds,
-      types: ['establishment']
-    };
+  // Set listener to the map for if the bounds change.
 
-    var place;
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
+  var input = document.getElementById("searchTextField");
+  var options = {
+    bounds: defaultBounds,
+    types: ["establishment"]
+  };
 
-    autocomplete.addListener('place_changed', function () {
+  var searchBox = new google.maps.places.SearchBox(input, options);
 
-      var place = autocomplete.getPlace();
-      console.log(place);
-      var marker = new google.maps.Marker({
-        position: place.geometry.location,
-        map: map,
-        title: place.name
-      });
+  searchBox.addListener("place_changed", function() {
+    console.log(`Listener fired`);
+  });
 
-      var locationContent = `
+  markers.forEach(function(marker) {
+    marker.setMap(null);
+  });
+  markers = [];
+
+  var icon = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+
+  var places = searchBox.getPlaces();
+  console.log(`Did we get anything? :: ${places}`);
+  marker.push(
+    new google.maps.Marker({
+      position: place.geometry.location,
+      map: map,
+      icon: icon,
+      title: place.name
+    })
+  );
+
+  var locationContent = `
       <h1 class='js-place-headline'>${place.name}</h1>
-      <div class="js-place-address"><strong>Address:</strong> ${place.formatted_address}</div>
+      <div class="js-place-address"><strong>Address:</strong>
+      ${place.formatted_address}</div>
 
       <form>Save location: <input id="save-trigger" type="button" name="save" value="save"></form>
       `;
 
-      marker.addListener('click', function() {
-        var infowindow = new google.maps.InfoWindow({
-          content: locationContent
-          });
-        $('body').on('click', '#save-trigger', event => {
-          saveLocation(locationContent);
-          locationContent = ``;
-        });
-
-        infowindow.open(map, marker);
-        });
-
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
-
-      return;
-
+  marker.addListener("click", function() {
+    var infowindow = new google.maps.InfoWindow({
+      content: locationContent
+    });
+    $("body").on("click", "#save-trigger", event => {
+      saveLocation(locationContent);
+      locationContent = ``;
     });
 
-  };
+    infowindow.open(map, marker);
+  });
+
+  map.setCenter(place.geometry.location);
+  map.setZoom(17);
+  marker.setPosition(place.geometry.location);
+  marker.setVisible(true);
+
+  return;
+}
 
 function saveLocation(location) {
   console.log(`I passed to Saving and brought with me ${location}.`);
-  $('.js-saved-list').append(location);
+  $(".js-saved-list").append(location);
   console.log(`Did we do the thing?.`);
 }
 
