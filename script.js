f0907b908af74fce411aa3bda70ac3e3bedbc43a
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2019-03-08 20:34:20 -0500

    Clean up push
    Cleanig up commits

--- script.js
+++ script.js
@@ -4,19 +4,15 @@ var infowindow;
 var markers = [];
 var place;
 
-<<<<<<< HEAD
-const savedPlaces = [];
-=======
 function loadPage() {
-  eventListner();
-  initAutocomplete();
   initMap();
 }
 
-function initAutocomplete () {
-  var defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(40.712775,-74.005973),
-    new google.maps.LatLng(41, 75));
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.712, lng: -74.005 },
+    zoom: 8
+  });
 
   var input = document.getElementById('searchTextField');
   var options = {
@@ -25,40 +21,6 @@ function initAutocomplete () {
   };
 
   autocomplete = new google.maps.places.Autocomplete(input, options);
-
-}
-
-function initMap(searchTerm) {
->>>>>>> parent of 17fe506... Enabled auto complete, passing selected location, and adding a marker
-
-function loadPage() {
-  loadList();
-  initMap();
-  console.log(`Function loadPage() was loaded.`);
-}
-
-function loadList() {
-  console.log(`Function loadList() was loaded.`);
-  $("#js-clear-list").on("click", event => {
-    $(".js-saved-list").empty();
-    console.log(`Clear list ran.`);
-  });
-}
-
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.712, lng: -74.005 },
-    zoom: 8
-  });
-
-  // Set listener to the map for if the bounds change.
-
-  var input = document.getElementById("searchTextField");
-  var options = {
-    bounds: defaultBounds,
-    types: ["establishment"]
-  };
 
   var searchBox = new google.maps.places.SearchBox(input, options);
 
@@ -98,12 +60,11 @@ function initMap() {
       <form>Save location: <input id="save-trigger" type="button" name="save" value="save"></form>
       `;
 
-
   marker.addListener("click", function() {
     var infowindow = new google.maps.InfoWindow({
       content: locationContent
     });
-<<<<<<< HEAD
+
     $("body").on("click", "#save-trigger", event => {
       saveLocation(locationContent);
       locationContent = ``;
@@ -120,64 +81,18 @@ function initMap() {
   return;
 }
 
+function loadList() {
+  console.log(`Function loadList() was loaded.`);
+  $("#js-clear-list").on("click", event => {
+    $(".js-saved-list").empty();
+    console.log(`Clear list ran.`);
+  });
+}
+
 function saveLocation(location) {
   console.log(`I passed to Saving and brought with me ${location}.`);
   $(".js-saved-list").append(location);
   console.log(`Did we do the thing?.`);
 }
-      marker.addListener('click', function() {
-        var infowindow = new google.maps.InfoWindow({
-          content: locationContent
-          });
-
-        infowindow.open(map, marker);
-
-        $('#save-trigger').click(function () {
-          console.log("I DID run the listner.");
-          alert("I was clicked!");
-        });
-      });
-
-
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
-
-      return;
-    });
-
-};
 
 $(initMap);
-=======
-}
-
-function eventListner() {
-
-  $("#js-search-submit").on('click', function(event)  {
-  console.log('Clicked!');
-  event.preventDefault();
-  console.log('Event prevented!');
-  let userSearchTerm = $('#searchTextField').val();
-  updateMap(userSearchTerm);
-  });
-}
-
-function updateMap(searchTerm) {
-  console.log(`I passed(${searchTerm}) to updateMap`);
-  console.log(searchTerm);
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.712, lng: -74.005},
-    zoom: 8
-    });
-
-  var marker = new google.maps.Marker({
-   position: searchTerm,
-   map: map,
-   title: 'Hello World!'
-});
-}
-
-$(loadPage);
->>>>>>> parent of 17fe506... Enabled auto complete, passing selected location, and adding a marker
